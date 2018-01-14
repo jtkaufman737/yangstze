de88970c4411cdabd95947b23e44058db9bd00ba
Joy <jtkaufman737@gmail.com>
2018-01-14 17:19:24 -0500

    delete extra files

--- yangstze/yangstzeScrapy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from yangstzeScrapy import views
-
-urlpatterns=[
- url(r'^$',views.HomePageView.as_view()),
- url(r'^$',views.AboutPageView.as_view()),
- url(r'^$',views.HeadlinesPageView.as_view())
-]
--- yangstze/yangstzeapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from yangstzeapp import views
-from yangstzeScrapy import views
-
-urlpatterns=[
- url(r'^$',views.HomePageView.as_view()),
- url('^about/$',views.AboutPageView.as_view())
- ]

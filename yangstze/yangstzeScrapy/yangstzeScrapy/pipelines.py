a6a6d4a3870bacf0227f349ed057f8f800c47732
Joy <jtkaufman737@gmail.com>
2018-01-14 14:46:09 -0500

    Adding blog-esque changes and screenshots

--- yangstze/yangstzeScrapy/yangstzeScrapy/pipelines.py
+++ yangstze/yangstzeScrapy/yangstzeScrapy/pipelines.py
@@ -11,22 +11,22 @@ class YangstzescrapyPipeline(object):
         return item
 
   # JTK Begin work
-
-  def __init__(self):
-      self.file = open("headlinesdata.csv","wb")
-      self.exporter = Csv.ItemExporter(self.file, unicode)
-      self.exporter.start_exporting()
-
-  def close_spider(self, spider):
-      self.exporter.finish_exporting()
-      self.file.close()
-
-  def process_item(self, item, spider):
-      self.exporter.export_item(item)
-      return item
-
-  def create_valid_csv(self, item):
-     for key, value in item.items():
-         is_string = (isinstance(value, basestring))
-         if (is_string and ("," in value.encode('utf-8'))):
-             item[key] = "\"" + value + "\""
+  # 
+  # def __init__(self):
+  #     self.file = open("headlinesdata.csv","wb")
+  #     self.exporter = Csv.ItemExporter(self.file, unicode)
+  #     self.exporter.start_exporting()
+  #
+  # def close_spider(self, spider):
+  #     self.exporter.finish_exporting()
+  #     self.file.close()
+  #
+  # def process_item(self, item, spider):
+  #     self.exporter.export_item(item)
+  #     return item
+  #
+  # def create_valid_csv(self, item):
+  #    for key, value in item.items():
+  #        is_string = (isinstance(value, basestring))
+  #        if (is_string and ("," in value.encode('utf-8'))):
+  #            item[key] = "\"" + value + "\""

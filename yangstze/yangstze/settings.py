d95edb3cc14112716559f4ac127d226c2f1ec952
Joy <jtkaufman737@gmail.com>
2018-01-14 15:44:07 -0500

    Additional fix to settings.py

--- yangstze/yangstze/settings.py
+++ yangstze/yangstze/settings.py
@@ -127,3 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
